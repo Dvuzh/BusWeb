14dda2b9b2e01c8c73a39cc08a9e5092944fd2c5
Katya Dvuzhilova <katyushkadvuzhi90@gmail.com>
2019-04-09 12:31:26 +0600

    test

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import axios from "axios";
 // alex: вообще настройки для расширения в будущем необходимо на окружениях не использовать, для этого можно чекать переменную окружения, например
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-axios.defaults.baseURL = (process.env.NODE_ENV !== 'production') ? 'http://localhost:3001' : 'http://bus.dextudio.com:3001';
+axios.defaults.baseURL = (process.env.NODE_ENV !== 'production') ? 'http://localhost:3001' : 'http://bus.dextudio.com:2222';
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
